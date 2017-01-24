90342fbe97c3a958b6aef4352a319d83f5004e63
Peter Clemenko <peter@cyvr.io>
2017-01-23 23:12:20 -0500

    ip6 supportt and ignore list for nmap

--- parsers/ParseNMapXMLToReport.py
+++ parsers/ParseNMapXMLToReport.py
@@ -10,10 +10,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument("nmapxml")
 args = parser.parse_args()
 
+ignoreFile = "ignore.txt"
+
+try:
+    fh = open(ignoreFile,'r')
+except:
+# if file does not exist, create it
+    fh = open(ignoreFile,'w')
 
 nmapXmlFile = args.nmapxml
 
 f = open('nmapReport.txt', 'w')
+
+with open(ignoreFile) as igf:
+    ignorelist = igf.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+ignorelist = [x.strip() for x in ignorelist] 
 
 nmap_report = NmapParser.parse_fromfile(nmapXmlFile)
 for NmapHost in nmap_report.hosts:
@@ -21,15 +33,26 @@ for NmapHost in nmap_report.hosts:
     ip4 = NmapHost.ipv4
     ip6 = NmapHost.ipv6
     mac = NmapHost.mac
-    services = NmapHost.services
-    for serv in services:
-            data = {}
-            ip4 = NmapHost.ipv4
-            ip6 = NmapHost.ipv6
-            mac = NmapHost.mac
-            port = serv.port
-            proto = serv.protocol
-            state = serv.state
-            f.write(ip4 + ":" + str(port) + "/" + proto + '\n')  # python will convert \n to os.linesep
+    if ip4:
+        if(ip4 not in ignorelist):
+            services = NmapHost.services
+            for serv in services:
+                data = {}
+                ip4 = NmapHost.ipv4
+                port = serv.port
+                proto = serv.protocol
+                state = serv.state
+                f.write(ip4 + ":" + str(port) + "/" + proto + '\n')  # python will convert \n to os.linesep
+
+    if ip6:
+        if(ip6 not in ignorelist):
+            services = NmapHost.services
+            for serv in services:
+                data = {}
+                ip6 = NmapHost.ipv6
+                port = serv.port
+                proto = serv.protocol
+                state = serv.state
+                f.write(ip6 + ":" + str(port) + "/" + proto + '\n')  # python will convert \n to os.linesep
 
 f.close()  # you can omit in most cases as the destructor will call it
